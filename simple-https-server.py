f51582b395336767bf603a6892305686197e18be
Pinaki Poddar <pinaki.poddar@gmail.com>
2024-11-13 11:39:38 +0530

    change css layout

--- simple-https-server.py
+++ simple-https-server.py
@@ -10,5 +10,5 @@ import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='server.pem', server_side=True)
 httpd.serve_forever()
