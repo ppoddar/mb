9a0ba1384e18f2acdb5a14699508a71dcb19d2b6
Pinaki Poddar <pinaki.poddar@gmail.com>
2024-11-26 13:04:21 +0530

    use dns entry not ip address

--- connect-ec2.sh
+++ connect-ec2.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 REMOTE_USER=ec2-user
-REMOTE_HOST=54.193.158.233
+#REMOTE_HOST=54.193.158.233
+REMOTE_HOST=mahabharatha.in
 PEM_FILE=digital-artisan.pem
 ssh -i $PEM_FILE $REMOTE_USER@$REMOTE_HOST
